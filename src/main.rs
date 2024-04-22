b4d594a82d92a469f524ac29641453665f4229eb
JoshdfG <agboolajoshua161@gmail.com>
2024-04-22 12:45:43 +0100

    struct and implentation chapter

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+#[derive(Debug)]
+struct Rectangle{
+    width:u32,
+    height:u32,
+}
+
+impl Rectangle {
+    fn area(&self)->u32{
+        self.width * self.height
+    }
+
+    fn double_width(&mut self) ->u32{
+        self.width *2 
+    }
+}
+
+fn main(){
+    let rect1 = (20,30);
+    
+
+    let rect2 = Rectangle {
+        width:30,
+        height:70,
+    };
+
+    let impl_struct = Rectangle{
+        width : 40,
+        height : 50,
+    };
+
+    println!("The width of the rectangle implentation is {} sqm.",impl_struct.area());
+
+
+    println!("The area of the rectangle is {} square metres.", rect(rect1));
+
+    println!("The area of the rectangle2 is {:?} sqm.",rect2);
+
+}
+
+fn rect(dimensions:(u32,u32))-> u32{
+    dimensions.0 * dimensions.1
+}
+
+fn rectwan(rectangle:&Rectangle)-> u32 {
+    rectangle.width * rectangle.height
+}
+
