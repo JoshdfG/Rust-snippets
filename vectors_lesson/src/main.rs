e5e3ec28df685b5d2acc79f00ef6b9d5b7a32fe3
JoshdfG <agboolajoshua161@gmail.com>
2024-06-10 23:34:53 +0100

    feat: rust error handling with closures

--- vectors_lesson/src/main.rs
+++ vectors_lesson/src/main.rs
@@ -1,4 +1,6 @@
 use ::std::collections::HashMap;
+use core::panic;
+use std::{fs::File, io::ErrorKind};
 fn main() {
     let mut v = vec![1, 2, 3, 4, 5];
 
@@ -105,6 +107,88 @@ fn main() {
     let u = "qqqybuwwww";
     let counter = repetition(u);
     println!("{}", counter);
+
+    let condition = false;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {number}");
+    // this will fail because the file we are trying to open does not exist
+
+    let greeting_file_result = File::open("hello.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
+
+    // but this will pass because the file doesnt exist but then we created it to make the error
+    // fail successfully
+    let greeting_file_result = File::open("hello.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error);
+            }
+        },
+    };
+
+    let file_creation = File::create("hi.txt");
+
+    let fc = match file_creation {
+        Ok(file) => file,
+        Err(er) => match er.kind() {
+            ErrorKind::NotFound => match File::create("hi.txt") {
+                Ok(f) => f,
+                Err(e) => panic!("error {:?}", e),
+            },
+            other => {
+                panic!("err {:?}", other)
+            }
+        },
+    };
+
+    // another method using unwrap_or_else and closures
+    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+
+    {
+        let get_file = File::create("hello.txt");
+        let fs_p = match get_file {
+            Ok(pass) => pass,
+            Err(err) => match err.kind() {
+                ErrorKind::NotFound => match File::create("hello.txt") {
+                    Ok(fs) => fs,
+                    Err(err) => panic!("error {:?}", err),
+                },
+                other => {
+                    panic!("other error {:?}", other);
+                }
+            },
+        };
+
+        // the same expression using closures
+        let f = File::open("durh.txt").unwrap_or_else(|error| {
+            if error.kind() == ErrorKind::NotFound {
+                File::create("durh.txt")
+                    .unwrap_or_else(|error| panic!("unable to create the file {:?}", error))
+            } else {
+                panic!("unknown error occured{:?}", error)
+            }
+        });
+    }
 }
 //sequence n!=1
 fn sequence(mut n: u64) -> (u64, u32) {
@@ -121,8 +205,8 @@ fn sequence(mut n: u64) -> (u64, u32) {
 }
 
 fn repetition(dna_sequence: &str) -> usize {
-    let mut max_length = 0;
     let mut current_length = 0;
+    let mut max_length = 0;
 
     for (i, c) in dna_sequence.chars().enumerate() {
         if i > 0 && dna_sequence.chars().collect::<Vec<_>>()[i - 1] != c {
