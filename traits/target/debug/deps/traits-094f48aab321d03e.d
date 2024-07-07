f6c937717d019e9f2321fff3405b05ee103b0743
JoshdfG <agboolajoshua161@gmail.com>
2024-07-07 15:15:51 +0100

    feat:traits

---
+++ traits/target/debug/deps/traits-094f48aab321d03e.d
@@ -0,0 +1,5 @@
+/home/josh/Projects/Rust/Rust-snippets/traits/target/debug/deps/libtraits-094f48aab321d03e.rmeta: src/main.rs
+
+/home/josh/Projects/Rust/Rust-snippets/traits/target/debug/deps/traits-094f48aab321d03e.d: src/main.rs
+
+src/main.rs:
---
+++ traits/target/debug/deps/traits-6d67743a8bb22585.d
@@ -0,0 +1,5 @@
+/home/josh/Projects/Rust/Rust-snippets/traits/target/debug/deps/libtraits-6d67743a8bb22585.rmeta: src/main.rs
+
+/home/josh/Projects/Rust/Rust-snippets/traits/target/debug/deps/traits-6d67743a8bb22585.d: src/main.rs
+
+src/main.rs:
