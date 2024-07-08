62727ed5a8ca748b40092befd12d2788834c06dc
JoshdfG <agboolajoshua161@gmail.com>
2024-07-08 01:25:42 +0100

    feat: added examples to understand traits and generics

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,3 +1,72 @@
+use std::ops::Not;
+
+#[derive(Debug)]
+struct Person<PetType, PetType1>
+where
+    PetType: Animal + NotDangerous,
+    PetType1: Dangerous + Animal,
+{
+    name: String,
+    pet: PetType,
+    pet2: PetType1,
+}
+
+trait Animal {
+    fn make_sound(&self) -> ();
+}
+
+trait NotDangerous {}
+
+trait Dangerous {}
+struct Cat {}
+
+impl NotDangerous for Cat {}
+impl Animal for Cat {
+    fn make_sound(&self) -> () {
+        println!("Cat meowed!");
+    }
+}
+
+#[derive(Debug)]
+struct Dog {}
+impl NotDangerous for Dog {}
+impl Animal for Dog {
+    fn make_sound(&self) -> () {
+        println!("Dog barked!");
+    }
+}
+struct Tiger {}
+
+impl Dangerous for Tiger {}
+
+impl Animal for Tiger {
+    fn make_sound(&self) -> () {
+        println!("Tiger Roared!");
+    }
+}
+struct Bear {}
+impl Dangerous for Bear {}
+impl Animal for Bear {
+    fn make_sound(&self) -> () {
+        println!("Bear roared!");
+    }
+}
+
+pub fn create_person() {
+    let pet1 = Dog {};
+    let pet2 = Cat {};
+    let pet3 = Bear {};
+    let pet4 = Tiger {};
+
+    let p1 = Person {
+        name: String::from("Trevor"),
+        pet: pet2,
+        pet2: pet3,
+    };
+    p1.pet.make_sound();
+    p1.pet2.make_sound();
+}
+
 fn main() {
-    println!("Hello, world!");
+    create_person()
 }
---
+++ traits/target/debug/traits.d
@@ -0,0 +1,1 @@
+/home/josh/Projects/Rust/Rust-snippets/traits/target/debug/traits: /home/josh/Projects/Rust/Rust-snippets/traits/src/main.rs
